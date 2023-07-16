oriontvv/k8s-openapi

commit c840bb410cc32b0620b605ed58c392933372ceea

Implement deep merge strategies according to the OpenAPI spec.
Fixes #128
Closes #138

k8s-openapi-codegen-common/src/lib.rs
@@ -319,6 +319,7 @@ pub fn run(
 						field_type_name,
 						required,
 						is_flattened,
+						merge_type: &schema.merge_type,
 					});
 				}
 
@@ -666,6 +667,12 @@ pub fn run(
 				where_part: Some(&template_generics_where_part),
 			};
 
+			let items_merge_type = swagger20::MergeType::List {
+				strategy: swagger20::KubernetesListType::Map,
+				keys: vec!["metadata().namespace".to_string(), "metadata().name".to_string()],
+				item_merge_type: Box::new(swagger20::MergeType::Default),
+			};
+
 			let template_properties = vec![
 				templates::Property {
 					name: "items",
@@ -674,6 +681,7 @@ pub fn run(
 					field_type_name: "Vec<T>".to_owned(),
 					required: templates::PropertyRequired::Required { is_default: true },
 					is_flattened: false,
+					merge_type: &items_merge_type,
 				},
 
 				templates::Property {
@@ -683,6 +691,7 @@ pub fn run(
 					field_type_name: (*metadata_rust_type).to_owned(),
 					required: templates::PropertyRequired::Required { is_default: true },
 					is_flattened: false,
+					merge_type: &swagger20::MergeType::Default,
 				},
 			];
 
@@ -729,6 +738,12 @@ pub fn run(
 			)?;
 
 			if definition.impl_deep_merge {
+				let template_generics_where_part = format!("T: {local}DeepMerge + {local}Metadata<Ty = {local}apimachinery::pkg::apis::meta::v1::ObjectMeta> + {local}ListableResource");
+				let template_generics = templates::Generics {
+					where_part: Some(&template_generics_where_part),
+					..template_generics
+				};
+
 				templates::struct_deep_merge::generate(
 					&mut out,
 					type_name,
@@ -808,6 +823,7 @@ pub fn run(
 						field_type_name,
 						required: templates::PropertyRequired::Optional,
 						is_flattened: false,
+						merge_type: &schema.merge_type,
 					});
 				}
 
@@ -1008,7 +1024,7 @@ pub fn run(
 					schema_feature,
 					map_namespace,
 				)?;
-			} 
+			}
 
 			swagger20::SchemaKind::Ty(swagger20::Type::WatchEvent(_)) => {
 				templates::impl_schema::generate(
@@ -1441,7 +1457,7 @@ pub fn get_rust_ident(name: &str) -> std::borrow::Cow<'static, str> {
 		}
 		else {
 			result.push(match c {
-				'.' | '-' => '_',
+				'-' => '_',
 				c => c,
 			});
 		}
@@ -1691,7 +1707,7 @@ pub fn write_operation(
 					path == "io.k8s.ListOptional" ||
 					path == "io.k8s.PatchOptional" ||
 					path == "io.k8s.ReplaceOptional" ||
-					path == "io.k8s.WatchOptional" 
+					path == "io.k8s.WatchOptional"
 				}
 				else {
 					false

k8s-openapi-tests-macro-deps/src/lib.rs
@@ -39,7 +39,7 @@ impl k8s_openapi::serde::Serialize for FooBarSpec {
 impl k8s_openapi::DeepMerge for FooBarSpec {
 	fn merge_from(&mut self, other: Self) where Self: Sized {
 		self.prop1.merge_from(other.prop1);
-		self.prop2.merge_from(other.prop2);
+		k8s_openapi::merge_strategies::list::atomic(&mut self.prop2, other.prop2);
 		self.prop3.merge_from(other.prop3);
 	}
 }

src/lib.rs
@@ -428,7 +428,7 @@ pub use _byte_string::ByteString;
 
 #[path = "deep_merge.rs"]
 mod _deep_merge;
-pub use self::_deep_merge::DeepMerge;
+pub use self::_deep_merge::{DeepMerge, strategies as merge_strategies};
 
 #[path = "resource.rs"]
 mod _resource;

commit 935f6da15f11e2155571a558bfb5ee850bd35f1b

Fix converting '.' to '_' in idents.
3bfe7c098e6f6686d263e750605d14ef0ce6892e added the ability to handle names
containing `.` for the sake of OpenShift, but then this was inadvertently
broken by c840bb410cc32b0620b605ed58c392933372ceea . This went unnoticed
because the test added by the former was not being run.

ci/global.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 
 echo '### k8s-openapi-codegen-common:clippy ###'
 pushd k8s-openapi-codegen-common
+cargo test
 cargo clippy -- -D warnings
 popd
 

k8s-openapi-codegen-common/src/lib.rs
@@ -1457,7 +1457,7 @@ pub fn get_rust_ident(name: &str) -> std::borrow::Cow<'static, str> {
 		}
 		else {
 			result.push(match c {
-				'-' => '_',
+				'-' | '.' => '_',
 				c => c,
 			});
 		}